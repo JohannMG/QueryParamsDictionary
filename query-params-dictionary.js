3fb026ca7e05e44881d0a14673bee344f3f9cfa4
Johann Garces <hello@johannmg.com>
2016-09-28 21:33:30 -0400

    Scaffold for [failing tests]

---
+++ query-params-dictionary.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function Convert(){
+
+}
+/*
+    Given a URL, will create a dictionare of the param values
+    Will use use the entire string delimited between the query mark (?) and the fragment (#)
+    or the begining and ends of the string, repectively
+*/
+Convert.prototype.urlToDictionary = function (string) {
+    return ""
+}
+
+
+module.exports = Convert
---
+++ test/test-dict.js
@@ -0,0 +1,23 @@
+var assert = require('assert')
+, paramConvert = require('../query-params-dictionary');
+
+describe ('return of emptystring', function(){
+    describe('empty string', function(){
+        it ('should return nil on empty string', function(){
+            assert.equal(-1, 3);
+        });
+    });
+});
+
+/*
+
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
+
+*/
